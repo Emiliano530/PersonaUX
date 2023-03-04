b1945f927d54237addf70a5256906fb41a29c416
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-03-04 11:13:06 -0600

    Persona-UX

--- Persona-UX/src/components/Persona.vue
+++ Persona-UX/src/components/Persona.vue
@@ -342,7 +342,7 @@ export default {
         class="blocks absolute left-20 top-full w-10/12 h-14 justify-center text-center"
       >
         <button
-    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    class="bg-blue-500 hover:bg-blue-700 text-white mt-8 font-bold py-2 px-4 rounded"
     @click="submit()"
   >
     Submit
