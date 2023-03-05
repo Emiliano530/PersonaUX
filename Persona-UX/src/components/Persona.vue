76f1ace76a7a10b7bf470e35777699cd95f4f488
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-03-05 13:30:49 -0600

    Persona-UX

--- Persona-UX/src/components/Persona.vue
+++ Persona-UX/src/components/Persona.vue
@@ -2,64 +2,113 @@
 import iconEdad from "../components/icons/IconEdad.vue"
 import axios from "axios";
 let API_URL = "/api/guardarPersonasUxd.php";
-
 export default {
   components:{
     iconEdad,
   },
   data() {
     return {
-      nombre: "",
-      apellido: "",
-      edad: "",
-      estadoCivil: "",
-      trabajo: "",
-      residencia: "",
-      cita: "",
-      citaAutor: "",
-      bio: "",
-      personalidad01: "50",
-      personalidad02: "50",
-      personalidad03: "50",
-      personalidad04: "50",
-      objetivos: [],
-      frustraciones: [],
-      motivaciones: [],
-      porcentaje: "",
-      marcas: "",
-    };
+      datos:{
+        nombre: "",
+        apellido: "",
+        edad: "",
+        estadoCivil: "",
+        trabajo: "",
+        residencia: "",
+        cita: "",
+        citaAutor: "",
+        bio: "",
+        personalidad01: "50",
+        personalidad02: "50",
+        personalidad03: "50",
+        personalidad04: "50",
+        objetivos: [],
+        frustraciones: [],
+        motivaciones: [
+        {
+          id: '1',
+          motivacion: 'incentive',
+          porcentaje: '80',
+          persona_id: '1'
+        },
+        {
+          id: '2',
+          motivacion: 'fear',
+          porcentaje: '30',
+          persona_id: '1'
+        },
+        {
+          id: '3',
+          motivacion: 'social',
+          porcentaje: '80',
+          persona_id: '1'
+        },
+        {
+          id: '4',
+          motivacion: 'achievement',
+          porcentaje: '75',
+          persona_id: '1'
+        },
+        {
+          id: '5',
+          motivacion: 'growth',
+          porcentaje: '85',
+          persona_id: '1'
+        }
+    ],
+        marcas: "",
+      },
+    mensajeError: ""
+    }
   },
   methods: {
     submit() {
+      for (let key in this.datos) {
+        if (!this.datos[key]) {
+          window.scrollTo(0, 0);
+          this.mensajeError = "Se deben llenar todos los campos";
+          return;
+        }
+      }
+      window.scrollTo(0, 0);
       axios
         .post(API_URL, {
-          nombre: this.nombre+this.apellido,
-          edad: this.edad,
-          estadoCivil: this.estadoCivil,
-          trabajo: this.trabajo,
-          residencia: this.residencia,
-          cita: this.cita,
-          citaAutor: this.citaAutor,
-          bio: this.bio,
-          personalidad01: this.personalidad01,
-          personalidad02: this.personalidad02,
-          personalidad03: this.personalidad03,
-          personalidad04: this.personalidad04,
-          objetivos: this.objetivos,
-          frustraciones: this.frustraciones,
-          motivaciones: this.motivaciones,
-          porcentaje: this.porcentaje,
-          marcas: this.marcas,
+          nombre: this.datos.nombre + ' ' + this.datos.apellido,
+          edad: this.datos.edad,
+          estadoCivil: this.datos.estadoCivil,
+          trabajo: this.datos.trabajo,
+          residencia: this.datos.residencia,
+          cita: this.datos.cita,
+          citaAutor: this.datos.citaAutor,
+          bio: this.datos.bio,
+          personalidad01: this.datos.personalidad01,
+          personalidad02: this.datos.personalidad02,
+          personalidad03: this.datos.personalidad03,
+          personalidad04: this.datos.personalidad04,
+          objetivos: this.datos.objetivos,
+          frustraciones: this.datos.frustraciones,
+          //motivaciones: this.datos.motivaciones,
+          porcentaje: this.datos.porcentaje,
+          marcas: this.datos.marcas,
         })
         .then((response) => {
           console.log(response.status);
+          Object.assign(this.$data, this.$options.data.call(this));
+          console.log("Datos guardados");
         });
+      // Aquí realizarías la acción de guardar el objeto person
+    },
+    enInput() {
+      this.mensajeError = "";
     },
   },
 };
 </script>
 <template>
-  <div class="card relative flex p-2 bg-slate-900 m-16 mb-28">
+  <div class="grid grid-cols-1 m-2 place-items-center">
+    <div v-if="mensajeError" class="card bg-red-700 border border-blue-700 text-white text-center rounded-full w-2/5 mb-5 mt-5 p-6"><strong>{{ mensajeError }}</strong></div>
+  </div>
+  <div class="card relative flex p-2 bg-slate-900 m-16 mt-2 mb-28">
     <div class="card static w-3/12 bg-violet-900 ml-12 mt-8 mb-8">
       <div
         class="blocks absolute top-6 left-6 bg-gray-600 border-orange-700 border w-48 h-64"
@@ -70,7 +119,7 @@ export default {
           <input
             id="name"
             maxlength="100"
-            v-model="nombre"
+            v-model="datos.nombre" @focus="enInput"
             placeholder="Nombre"
             class="pl-3 pr-3 bg-violet-900 rounded text-white text-center placeholder-white text-lg border-none shadow outline-none focus:outline-none focus:ring w-11/12 mt-8"
             type="text"
@@ -78,7 +127,7 @@ export default {
           <input
             id="apellido"
             maxlength="100"
-            v-model="apellido"
+            v-model="datos.apellido" @focus="enInput"
             placeholder="Apellido"
             class="pl-3 pr-3 bg-violet-900 rounded text-white text-center placeholder-white text-lg border-0 shadow outline-none focus:outline-none focus:ring w-11/12 mb-24 mt-1"
             type="text"
@@ -92,12 +141,12 @@ export default {
               cols="40"
               rows="3"
               maxlength="500"
-              v-model="cita"
+              v-model="datos.cita" @focus="enInput"
             ></textarea>
             <input
               id="citaAutor"
               maxlength="500"
-              v-model="citaAutor"
+              v-model="datos.citaAutor" @focus="enInput"
               placeholder="Autor de la cita"
               class="pl-3 pr-3 bg-black rounded text-white text-end placeholder-white text-xs border-0 shadow outline-none focus:outline-none focus:ring w-11/12 mt-0.5 mb-2"
               type="textarea"
@@ -106,7 +155,7 @@ export default {
           <input
             id="edad"
             maxlength="2"
-            v-model="edad"
+            v-model="datos.edad" @focus="enInput"
             placeholder="Edad"
             class="pl-3 pr-3 bg-violet-900 rounded text-white text-center placeholder-white text-lg border-0 shadow outline-none focus:outline-none focus:ring w-11/12 mt-6"
             type="tel"
@@ -114,13 +163,13 @@ export default {
           <input
             id="residencia"
             maxlength="200"
-            v-model="residencia"
+            v-model="datos.residencia" @focus="enInput"
             placeholder="Residencia"
             class="pl-3 pr-3 bg-violet-900 rounded text-white text-center placeholder-white text-lg border-0 shadow outline-none focus:outline-none focus:ring w-11/12 mt-1"
             type="text"
           />
           <select
-            v-model="estadoCivil"
+            v-model="datos.estadoCivil" @focus="enInput"
             class="pl-3 pr-3 bg-violet-900 rounded text-white text-center placeholder-white text-lg border-0 shadow outline-none focus:outline-none focus:ring w-11/12 mt-1"
           >
             <option value="" class="bg-red-700 text-white">Estado Civil</option>
@@ -134,7 +183,7 @@ export default {
           <input
             id="trabajo"
             maxlength="200"
-            v-model="trabajo"
+            v-model="datos.trabajo" @focus="enInput"
             placeholder="Trabajo"
             class="pl-3 pr-3 bg-violet-900 rounded text-white text-center placeholder-white text-lg border-0 shadow outline-none focus:outline-none focus:ring w-11/12 mt-1 mb-4"
             type="text"
@@ -156,7 +205,7 @@ export default {
               cols="40"
               rows="8"
               maxlength="700"
-              v-model="bio"
+              v-model="datos.bio" @focus="enInput"
             ></textarea>
             </div>
           </div>
@@ -173,7 +222,7 @@ export default {
               cols="40"
               rows="3"
               maxlength="300"
-              v-model="objetivos"
+              v-model="datos.objetivos" @focus="enInput"
             ></textarea>
             </div>
             <label for="frustraciones"><strong>Frustraciones</strong></label>
@@ -186,7 +235,7 @@ export default {
               cols="40"
               rows="3"
               maxlength="300"
-              v-model="frustraciones"
+              v-model="datos.frustraciones" @focus="enInput"
             ></textarea>
             </div>
           </div>
@@ -201,14 +250,14 @@ export default {
             >Introvertido</span
           >
           <span class="text-center text-sm font-medium">{{
-            this.personalidad01
+            this.datos.personalidad01
           }}</span>
           <span class="text-end text-sm font-medium"
             >Extrovertido</span
           >
           </div>
           <input
-            v-model="personalidad01"
+            v-model="datos.personalidad01" @focus="enInput"
             type="range"
             min="0"
             max="100"
@@ -223,14 +272,14 @@ export default {
             >Observador</span
           >
           <span class="text-center text-sm font-medium">{{
-            this.personalidad02
+            this.datos.personalidad02
           }}</span>
           <span class="text-end text-sm font-medium"
             >Intuitivo</span
           >
           </div>
           <input
-            v-model="personalidad02"
+            v-model="datos.personalidad02" @focus="enInput"
             type="range"
             min="0"
             max="100"
@@ -245,14 +294,14 @@ export default {
             >Emocional</span
           >
           <span class="text-center text-sm font-medium">{{
-            this.personalidad03
+            this.datos.personalidad03
           }}</span>
           <span class="text-end text-sm font-medium"
             >Pensamiento</span
           >
           </div>
           <input
-            v-model="personalidad03"
+            v-model="datos.personalidad03" @focus="enInput"
             type="range"
             min="0"
             max="100"
@@ -267,14 +316,14 @@ export default {
             >Prospeccion</span
           >
           <span class="text-center text-sm font-medium">{{
-            this.personalidad04
+            this.datos.personalidad04
           }}</span>
           <span class="text-end text-sm font-medium"
             >Juzgador</span
           >
           </div>
           <input
-            v-model="personalidad04"
+            v-model="datos.personalidad04" @focus="enInput"
             type="range"
             min="0"
             max="100"
@@ -290,9 +339,9 @@ export default {
             <br />
             <div class="grid gap-2 grid-cols-5 mt-2">
               <label class="mr-8 mb-1 col-start-1 col-end-3" for="motivaciones">incentivo</label>
-              <span class="col-start-3 text-center col-end-4">h</span>
+              <span class="col-start-3 text-center col-end-4">{{ datos.motivaciones[0].porcentaje }}</span>
               <input
-                v-model="motivaciones.porcentaje1"
+                v-model="datos.motivaciones[0].porcentaje" @focus="enInput"
                 type="range"
                 min="0"
                 max="100"
@@ -300,9 +349,9 @@ export default {
                 class="w-4/4 h-3 col-start-4 col-span-2 rounded-lg mt-2 bg-gray-300 appearance-none focus:outline-none"
               />
               <label class="mr-8 mb-1 col-start-1 col-end-3" for="motivaciones">Miedo</label>
-              <span class="col-start-3 text-center col-end-4">h</span>
+              <span class="col-start-3 text-center col-end-4">{{ datos.motivaciones[1].porcentaje }}</span>
               <input
-                v-model="motivaciones.porcentaje2"
+                v-model="datos.motivaciones[1].porcentaje" @focus="enInput"
                 type="range"
                 min="0"
                 max="100"
@@ -310,9 +359,9 @@ export default {
                 class="w-4/4 h-3 col-start-4 col-span-2 rounded-lg mt-2 bg-gray-300 appearance-none focus:outline-none"
               />
               <label class="mr-8 mb-1 col-start-1 col-end-3" for="motivaciones">Social</label>
-              <span class="col-start-3 text-center col-end-4">h</span>
+              <span class="col-start-3 text-center col-end-4">{{ datos.motivaciones[2].porcentaje }}</span>
               <input
-                v-model="motivaciones.porcentaje3"
+                v-model="datos.motivaciones[2].porcentaje" @focus="enInput"
                 type="range"
                 min="0"
                 max="100"
@@ -320,9 +369,9 @@ export default {
                 class="w-4/4 h-3 col-start-4 col-span-2 rounded-lg mt-2 bg-gray-300 appearance-none focus:outline-none"
               />
               <label class="mr-8 mb-1 col-start-1 col-end-3" for="motivaciones">Logro</label>
-              <span class="col-start-3 text-center col-end-4">h</span>
+              <span class="col-start-3 text-center col-end-4">{{ datos.motivaciones[3].porcentaje }}</span>
               <input
-                v-model="motivaciones.porcentaje4"
+                v-model="datos.motivaciones[3].porcentaje" @focus="enInput"
                 type="range"
                 min="0"
                 max="100"
@@ -330,9 +379,9 @@ export default {
                 class="w-4/4 h-3 col-start-4 col-span-2 rounded-lg mt-2 bg-gray-300 appearance-none focus:outline-none"
               />
               <label class="mr-8 mb-1 col-start-1 col-end-3" for="motivaciones">Crecimiento</label>
-              <span class="col-start-3 text-center col-end-4">h</span>
+              <span class="col-start-3 text-center col-end-4">{{ datos.motivaciones[4].porcentaje }}</span>
               <input
-                v-model="motivaciones.porcentaje5"
+                v-model="datos.motivaciones[4].porcentaje" @focus="enInput"
                 type="range"
                 min="0"
                 max="100"
@@ -353,7 +402,7 @@ export default {
               id="marcas"
               cols="40"
               rows="7"
-              v-model="marcas"
+              v-model="datos.marcas" @focus="enInput"
             ></textarea>
             </div>
           </div>
